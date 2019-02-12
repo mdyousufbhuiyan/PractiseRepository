d45a27010711a52b51d5b3c98214bab690ee4270
Yousuf <mdyousuf336@gmail.com>
2019-02-12 15:38:51 +0600

    first ui commit

--- demoFile.js
+++ demoFile.js
@@ -6,3 +6,37 @@ var arr = [12, 12, 23, 24];
 arr.forEach(function(element) {
   console.log("element is : " + element);
 });
+
+arr.every(function(element) {
+  console.log("element : " + element);
+});
+
+function add(a, b, callback) {
+  callback(a, b);
+}
+
+function calculateGpa(number) {
+  var gpa;
+  if (number >= 80) {
+    gpa = "A+";
+  } else if (number >= 70) {
+    gpa = "A";
+  } else if (number >= 60) {
+    gpa = "A-";
+  } else if (number >= 50) {
+    gpa = "B";
+  } else if (number >= 40) {
+    gpa = "C";
+  } else {
+    gpa = "Failed";
+  }
+
+  return function(name) {
+    return "Hello " + name + " , Your gpa is : " + gpa;
+  };
+}
+
+var gpaReference = calculateGpa(35);
+console.log(gpaReference("md yousuf"));
+
+
